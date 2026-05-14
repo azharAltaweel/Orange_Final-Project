da8434f4fb3ae8377415b2b5e021b0eecbcb67a4
azharAltaweel <azharabdalmajeed@gmail.com>
2026-05-14 11:12:43 +0300

    add product info page (add+edit +delete)

--- Data/Migrations/20260514072218_AddDiscountToProduct.Designer.cs
+++ Data/Migrations/20260514072218_AddDiscountToProduct.Designer.cs
@@ -4,6 +4,7 @@ using E_commerce_Website__Skincare_.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace E_commerce_Website__Skincare_.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260514072218_AddDiscountToProduct")]
+    partial class AddDiscountToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
