59125d56d545be0141171f2d8a16dc19fb850a9e
Saeed <Saeed_Samer@outlook.com>
2026-05-15 18:49:26 +0300

    test identity and login & regsistretion

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using E_commerce_Website__Skincare_.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce_Website__Skincare_.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
